38edaeeb4298f1319aef812f03c7e1859f417e4d
oocex <lloginova551@gmail.com>
2024-04-25 17:50:20 +0300

    new

---
+++ bin/brain-prime.js
@@ -0,0 +1,19 @@
+#!/usr/bin/env node
+
+import func from '../src/index.js';
+
+const description = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const game = () => {
+  const randomNumber = Math.floor(Math.random() * 100) + 1;
+  const question = `${randomNumber}`;
+  let trueAnswer = 'yes';
+  for (let i = 2; i <= Math.sqrt(randomNumber); i += 1) {
+    if (randomNumber % i === 0) {
+      trueAnswer = 'no';
+      break;
+    }
+  }
+  return [question, trueAnswer];
+};
+func(description, game);
