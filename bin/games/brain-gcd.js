d871cac11578a4728a322bbe1e5a4a86353d28b7
oocex <lloginova551@gmail.com>
2024-04-25 17:21:11 +0300

    Committing local changes to brain-gcd.js

--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -3,24 +3,19 @@
 import func from '../src/index.js';
 
 const description = 'Find the greatest common divisor of given numbers.';
+
 const game = () => {
   const randomNumber1 = Math.floor(Math.random() * 100) + 1;
   const randomNumber2 = Math.floor(Math.random() * 100) + 1;
   const question = `${randomNumber1} ${randomNumber2}`;
   const numbers = [randomNumber1, randomNumber2];
-  const maxValue = (arr) => {
-    const [one, ..rest]
-    let max = arr [0]
-    for (const [one])
-  }
-  for (let i = 0; i <= Math.max.apply(null, numbers); i += 1) {
-    const result = [];
-    if (randomNumber1 % i === 0 && randomNumber1 % i === 0) {
-      result.push(i);
+  let trueAnswer;
+  for (let i = 1; i <= Math.max(numbers); i += 1) {
+    if (randomNumber1 % i === 0 && randomNumber2 % i === 0) {
+      trueAnswer = i;
     }
-    const trueAnswer = Math.max.apply(null, result);
   }
-  return [question, trueAnswer];
+  return [question, String(trueAnswer)];
 };
 
 func(description, game);
