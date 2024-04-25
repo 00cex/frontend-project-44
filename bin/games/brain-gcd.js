e1d9338de1c5c946f7bbc0865c0365624192e6cd
oocex <lloginova551@gmail.com>
2024-04-25 13:45:08 +0300

    new

--- bin/games/brain-gcd.js
+++ bin/games/brain-gcd.js
@@ -1,21 +1,26 @@
 #!/usr/bin/env node
-import { result } from 'lodash';
+
 import func from '../src/index.js';
 
-const description = 'Find the greatest common divisor of given numbers.'
+const description = 'Find the greatest common divisor of given numbers.';
 const game = () => {
-    const randomNumber1 = Math.floor(Math.random() * 100) + 1;
-    const randomNumber2 = Math.floor(Math.random() * 100) + 1;
-    const question = `${randomNumber1} ${randomNumber2}`
-    const numbers = [randomNumber1, randomNumber2]
-    for (let i = 0; i <= Math.max(numbers)) {
-        let result = []
-        if (randomNumber1 % i === 0 && randomNumber1 % i === 0) {
-            result.push(i)
-        }
-        let trueAnswer = Math.max(result)
+  const randomNumber1 = Math.floor(Math.random() * 100) + 1;
+  const randomNumber2 = Math.floor(Math.random() * 100) + 1;
+  const question = `${randomNumber1} ${randomNumber2}`;
+  const numbers = [randomNumber1, randomNumber2];
+  const maxValue = (arr) => {
+    const [one, ..rest]
+    let max = arr [0]
+    for (const [one])
+  }
+  for (let i = 0; i <= Math.max.apply(null, numbers); i += 1) {
+    const result = [];
+    if (randomNumber1 % i === 0 && randomNumber1 % i === 0) {
+      result.push(i);
     }
-    return [question, trueAnswer]
-}
+    const trueAnswer = Math.max.apply(null, result);
+  }
+  return [question, trueAnswer];
+};
 
-func(description, game)
+func(description, game);
