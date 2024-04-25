0c72efbd2c63c7c39936f75ba782980c51e58f4c
00cex <132567557+00cex@users.noreply.github.com>
2024-04-25 14:30:00 +0300

    Update brain-gcd.js

--- bin/brain-gcd.js
+++ bin/brain-gcd.js
@@ -8,9 +8,8 @@ const game = () => {
   const randomNumber1 = Math.floor(Math.random() * 100) + 1;
   const randomNumber2 = Math.floor(Math.random() * 100) + 1;
   const question = `${randomNumber1} ${randomNumber2}`;
-  const numbers = [randomNumber1, randomNumber2];
   let trueAnswer = 1;
-  for (let i = 1; i <= Math.max(numbers); i += 1) {
+  for (let i = 1; i <= Math.max(randomNumber1, randomNumber2); i += 1) {
     if (randomNumber1 % i === 0 && randomNumber2 % i === 0) {
       trueAnswer = i;
     }
