be91677b82ccee51fb3284260fbea3549210bad0
oocex <lloginova551@gmail.com>
2024-04-30 17:54:27 +0300

    new

---
+++ bin/brain-even.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+import func from '../src/index.js';
+
+const description = 'Answer "yes" if the number is even, otherwise answer "no".';
+
+const game = () => {
+  const isEven = (num) => num % 2 === 0;
+  const number = Math.floor(Math.random() * 100) + 1;
+  const trueAnswer = isEven(number) ? 'yes' : 'no';
+  const question = number.toString();
+  return [question, trueAnswer];
+};
+func(description, game);
---
+++ src/games/brain-calc.js
@@ -0,0 +1,22 @@
+import func from '../index.js';
+
+export default function runCalc() {
+  const description = 'What is the result of the expression?';
+  const game = () => {
+    const randomNumber1 = Math.floor(Math.random() * 100) + 1;
+    const randomNumber2 = Math.floor(Math.random() * 100) + 1;
+    const operations = ['+', '-', '*'];
+    const randomOperation = operations[Math.floor(Math.random() * operations.length)];
+    const question = `${randomNumber1}${randomOperation}${randomNumber2}`;
+    let trueAnswer;
+    if (randomOperation === '+') {
+      trueAnswer = randomNumber1 + randomNumber2;
+    } else if (randomOperation === '-') {
+      trueAnswer = randomNumber1 - randomNumber2;
+    } else {
+      trueAnswer = randomNumber1 * randomNumber2;
+    }
+    return [question, String(trueAnswer)];
+  };
+  func(description, game);
+}
